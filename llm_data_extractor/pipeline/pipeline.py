ef72a0339d5b7948e0cf7b785cfff5e1449d63f7
Esteban Cometto <estebancometto@gmail.com>
2024-08-20 21:02:23 -0300

    fix(release): update code

--- llm_data_extractor/pipeline/pipeline.py
+++ llm_data_extractor/pipeline/pipeline.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 import pydantic
 from pydantic.fields import FieldInfo
-from langchain_community.document_loaders import PyPDFLoader
 from langchain.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
